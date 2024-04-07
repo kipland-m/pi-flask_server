22e005c6ae5b53e40590df5f948d339f3c6670de
kipland-m <kiplandmelton@gmail.com>
2024-04-06 20:14:37 -0500

    adding folder for desktop pc files, created virtual environment for packages

---
+++ kipland-pc/readme.txt
@@ -0,0 +1,1 @@
+kipland-pc will hold python script to periodically send pc hardware info to flask server
---
+++ kipland-pc/send_hardware_data.py
@@ -0,0 +1,11 @@
+import requests
+import psutil
+import time
+
+kip_pi_ip = '192.168.0.73'
+
+while True:
+    try:
+
+    except Exception as e:
+        print("Error:", e)
